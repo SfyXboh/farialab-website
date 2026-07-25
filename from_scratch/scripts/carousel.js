48652ab70853d50592e3db73212ee3974bad6836
SfyXboh <rexswang17@gmail.com>
2026-07-25 13:57:36 -0400

    completed 1 carousel

--- from_scratch/scripts/carousel.js
+++ from_scratch/scripts/carousel.js
@@ -8,22 +8,43 @@ function showSlide(slides, captions, dots, i) {
     dots[i].classList.add('show');
 }
 
-function showNext(slides, captions, dots, currentslides, c) {
+function showPrev(slides, captions, dots, currentslides, c) {
+    currentslides[c] -= 1;
+    if (currentslides[c] < 0) {
+        currentslides[c] += slides.length;
+    }
+
     showSlide(slides, captions, dots, currentslides[c]);
+}
+
+function showNext(slides, captions, dots, currentslides, c) {
     currentslides[c] += 1;
     currentslides[c] %= slides.length;
+    
+    showSlide(slides, captions, dots, currentslides[c]);
 }
 
 let carousels = document.querySelectorAll('.carousel');
-let dots = document.querySelectorAll('.dots');
+let dotss = document.querySelectorAll('.dots');
 let currentslides = Array(carousels.length);
 
 /* initialize current slide */
 for (let c = 0; c < carousels.length; c++) {
-    currentslides[c] = 1;
+    currentslides[c] = 0;
 }
 
 /* auto next */
 for (let c = 0; c < carousels.length; c++) {
-    setInterval(() => showNext(carousels[c].querySelectorAll('.slide'), carousels[c].querySelectorAll('.caption'), dots[c].querySelectorAll('.dot'), currentslides, c), 10000);
+    let slides = carousels[c].querySelectorAll('.slide');
+    let captions = carousels[c].querySelectorAll('.caption');
+    let dots = dotss[c].querySelectorAll('.dot');
+    let prev = carousels[c].querySelector('.prev');
+    let next = carousels[c].querySelector('.next');
+
+    /* auto next */
+    setInterval(() => showNext(slides, captions, dots, currentslides, c), 10000);
+
+    /*manual next and previous buttons */
+    prev.addEventListener('click', () => showPrev(slides, captions, dots, currentslides, c));
+    next.addEventListener('click', () => showNext(slides, captions, dots, currentslides, c));
 }
